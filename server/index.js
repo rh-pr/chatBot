abb4f6026587d7b727108065f06c2a2ae955f3e4
Ruslana <ruslana.hural.j@gmail.com>
2024-12-04 17:07:54 +0100

    connected database

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 
 const app = express();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
 
 app.use(express.json());
 app.use(cors());
 
+
+
 app.listen(PORT, () => {
     console.log(`server run on this port ${PORT}`)
-})
+});
+
+mongoose.connect(uri).then(() => console.log('MongoDB contectes')).catch((error) => {console.log('MongoDB failed conection: ', error.message);});
