f5f1661d4f075151c5510f428544ae2452b96829
Ruslana <ruslana.hural.j@gmail.com>
2024-12-04 16:24:12 +0100

    created server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`server run on this port ${PORT}`)
+})
